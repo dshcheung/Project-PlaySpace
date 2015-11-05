2653c251890765a71101f5494d1d10bbc920d4b1
adaau <ada.au.cheng@gmail.com>
2015-11-05 09:48:15 +0800

    bookmarks working

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104032405) do
+ActiveRecord::Schema.define(version: 20151104162619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "playspace_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
